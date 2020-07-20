e02bb507cff961058dab3c854b303ead15fb57c8
shrNavi <68535278+shrNavi@users.noreply.github.com>
2020-07-20 13:50:51 +0530

    Create Assignment1.py

---
+++ Assignment1.py
@@ -0,0 +1,15 @@
+1.What is python? What are the ways to install puthon in pc?
+    Python is a high-level programming language designed to be easy to read and simple to implement. It is open source, which means it is free to use, even for commercial applications.
+Python was developed by Guido Van Rossum in 1991 and became popular in 2006 when google backed up and used it.
+Ways to install python:-
+-anaconda navigator
+-pycharm
+-python installer
+-thonny
+-VScode
+2.What are code environments and there types?
+    Code environment is a platform where one can write, compile and run a code.
+Types:-
+-Text editors like VS studio and atom.
+-Integrated Development environment(ide) like pycharm and spyder.
+-Notebook environment like jupyter
